0c665d3425b415dac0599edd41ef6b500271ec6a
Omar Ehab <113807991+OmarEhab262@users.noreply.github.com>
2024-09-19 19:22:20 +0300

    add who to get the comment by query

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,15 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
 import { comments } from "../data";
 
 // this git comment by id
-export async function GET(
-  _request: Request,
-  { params }: { params: { id: string } }
-) {
-  const comment = comments.find(
-    (comment) => comment.id === parseInt(params.id)
-  );
-  return Response.json(comment);
-}
+// export async function GET(
+//   _request: Request,
+//   { params }: { params: { id: string } }
+// ) {
+//   const comment = comments.find(
+//     (comment) => comment.id === parseInt(params.id)
+//   );
+//   return Response.json(comment);
+// }
 // this update the comment by id
 export async function PATCH(
   request: Request,
@@ -35,4 +36,21 @@ export async function DELETE(
   // const deletedComment = comments[index];
   comments.splice(index, 1);
   return Response.json(comments);
+}
+
+// this get the comment by query
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+
+  // Filter comments if the query exists
+  const filteredComments = query
+    ? comments.filter((comment) =>
+        comment.text.toLowerCase().includes(query.toLowerCase())
+      )
+    : comments;
+
+  // Return the filtered comments as JSON
+  return NextResponse.json(filteredComments);
 }
