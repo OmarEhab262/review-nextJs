d3001a6065d5e17e49ad9636100e22d2cbfd4092
Omar Ehab <113807991+OmarEhab262@users.noreply.github.com>
2024-09-19 12:25:52 +0300

    add patch and delete

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,5 +1,6 @@
 import { comments } from "../data";
 
+// this git comment by id
 export async function GET(
   _request: Request,
   { params }: { params: { id: string } }
@@ -8,4 +9,30 @@ export async function GET(
     (comment) => comment.id === parseInt(params.id)
   );
   return Response.json(comment);
+}
+// this update the comment by id
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const { text } = body;
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  comments[index].text = text;
+  return Response.json(comments[index]);
+}
+// this delete the comment by id
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  // if u want return the deleted comment
+  // const deletedComment = comments[index];
+  comments.splice(index, 1);
+  return Response.json(comments);
 }
